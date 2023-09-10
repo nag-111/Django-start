05650aa54a3ffe1879d5bbbd9d4dd5a88da915f5
nag-111 <nagarajukotagiri111@gmail.com>
2023-09-10 19:57:38 +0530

    models added

--- App1/admin.py
+++ App1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Blog,Personal_details
 # Register your models here.
+
+admin.site.register(Blog)
+admin.site.register(Personal_details)
--- App1/models.py
+++ App1/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images/")
+    description = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.title
+    
+class Personal_Details(models.Model):
+    Name = models.CharField(max_length=50)
+    Age = models.IntegerField(len=2)
+    Address = models.TextField
+    Pincode = models.IntegerField(max_length=6) 
+
+    def __str__(self) -> str:
+        return self.Pincode
+
+class  
