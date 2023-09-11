ab70fbb2589ca385eff7f1bbc2efdb1f799ffd01
nag-111 <nagarajukotagiri111@gmail.com>
2023-09-11 09:04:37 +0530

    getting data from db to tempate

--- App1/models.py
+++ App1/models.py
@@ -4,9 +4,9 @@ from datetime import datetime
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
     image = models.ImageField(upload_to="images/")
     description = models.TextField()
-    author = models.CharField(max_length=100)
     created_at = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
